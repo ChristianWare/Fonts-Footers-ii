950ced5d1bb60db339ba1a198eb97f3ce538f228
Chris Ware <c.p.ware@hotmail.com>
2024-02-20 23:55:11 -0700

    Arrows

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import LayoutWrapper from "../LayoutWrapper";
 import styles from "./Hero.module.css";
 import Img1 from "../../../public/images/hero.jpg";
+import Arrow from "../../../public/icons/arrow.svg";
 
 const Hero = () => {
   return (
@@ -9,6 +10,11 @@ const Hero = () => {
       <LayoutWrapper>
         <div className={styles.top}>
           <div className={styles.topLeft}>
+            <div className={styles.arrowContainer}>
+              <Arrow className={styles.icon} />
+              <Arrow className={styles.icon} />
+              <Arrow className={styles.icon} />
+            </div>
             <p className={styles.copy}>
               Tired of relying solely on platforms like Airbnb or VRBO? We
               empower property owners to take control of their bookings with a
