81d7bfd46238ca8bc70c222b272290f10a06ae40
Chris Ware <c.p.ware@hotmail.com>
2024-02-20 18:06:13 -0700

    Hero Started

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import LayoutWrapper from "../LayoutWrapper";
+import styles from "./Hero.module.css";
+import Img1 from "../../../public/images/hero.jpg";
+
+const Hero = () => {
+  return (
+    <section className={styles.bgColor}>
+      <LayoutWrapper>
+        <div className={styles.top}>
+          <div className={styles.topLeft}>
+            <p className={styles.copy}>
+              Tired of relying solely on platforms like Airbnb or VRBO? We
+              empower property owners to take control of their bookings with a
+              custom website to showcase thier unique offerings.
+            </p>
+          </div>
+          <div className={styles.topRight}>
+            <h1 className={styles.heading}>Direct Booking Solutions</h1>
+          </div>
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.imgContainer}>
+            <Image src={Img1} alt='hero' fill className={styles.img} />
+          </div>
+          <div className={styles.bottomRight}>
+            <div className={styles.bottomRight1}></div>
+            <div className={styles.bottomRight2}></div>
+          </div>
+        </div>
+      </LayoutWrapper>
+    </section>
+  );
+};
+export default Hero;
