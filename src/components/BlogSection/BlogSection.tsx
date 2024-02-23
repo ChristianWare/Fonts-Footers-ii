99cfb64dfcdf57bbbfce7e77a46ca753762716bd
Chris Ware <c.p.ware@hotmail.com>
2024-02-23 13:52:48 -0700

    c

--- src/components/BlogSection/BlogSection.tsx
+++ src/components/BlogSection/BlogSection.tsx
@@ -5,7 +5,12 @@ const BlogSection = () => {
   return (
     <section>
       <LayoutWrapper>
-        <h2 className={styles.header}>Insights from Fonts & Footers</h2>
+        <div className={styles.top}>
+          <h2 className={styles.header}>Insights from Fonts & Footers</h2>
+        </div>
+        <div className={styles.bottom}>
+          
+        </div>
       </LayoutWrapper>
     </section>
   );
