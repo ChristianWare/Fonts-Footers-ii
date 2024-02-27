d8a2070eb8b9e3723c94b6b753f86aa2ce21d933
Chris Ware <c.p.ware@hotmail.com>
2024-02-27 00:30:07 -0700

    time

--- src/components/BlogIntro/BlogIntro.tsx
+++ src/components/BlogIntro/BlogIntro.tsx
@@ -15,38 +15,39 @@ interface BlogIntroProps {
 const BlogIntro: React.FC<BlogIntroProps> = ({ text, desc, src, date }) => {
   return (
     <section className={styles.container}>
-      <div className={styles.top}>
-        <h1 className={styles.heading}>{text}</h1>
-        <div className={styles.arrowContainer}>
           <ArrowCluster />
+      <div className={styles.top}>
+        <div className={styles.left}>
+          <h1 className={styles.heading}>{text}</h1>
+          <p className={styles.desc}>{desc}</p>
+          <div className={styles.authorDate}>
+            <div className={styles.left}>
+              <Image
+                src={Author}
+                alt='Author'
+                width={60}
+                height={60}
+                className={styles.author}
+              />
+            </div>
+            <div className={styles.right}>
+              <p className={styles.authName}>Chris Ware</p>
+              <p className={styles.date}>{date}</p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className={styles.bottom}>
-        <p className={styles.desc}>{desc}</p>
-        <div className={styles.imgContainer}>
-          <Image
-            src={urlFor(src).url()}
-            alt='image'
-            fill
-            className={styles.img}
-          />
-        </div>
-        <div className={styles.authorDate}>
-          <div className={styles.left}>
+        <div className={styles.right}>
+          <div className={styles.imgContainer}>
             <Image
-              src={Author}
-              alt='Author'
-              width={60}
-              height={60}
-              className={styles.author}
+              src={urlFor(src).url()}
+              alt='image'
+              fill
+              className={styles.img}
             />
           </div>
-          <div className={styles.right}>
-            <p className={styles.authName}>Chris Ware</p>
-            <p className={styles.date}>{date}</p>
-          </div>
         </div>
       </div>
+      <div className={styles.bottom}></div>
     </section>
   );
 };
