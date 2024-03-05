62ab6dfa7316d094ed27ae967c8e8bd0ee65ded5
Chris Ware <c.p.ware@hotmail.com>
2024-03-05 10:56:54 -0700

    Layout Grid fixed

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -74,86 +74,93 @@ function Nav() {
   const pathname = usePathname();
 
   return (
-    <header className={styles.header}>
+    <>
       <TopNav />
-      <nav className={styles.navbar}>
-        <ul
-          className={
-            isOpen === false
-              ? styles.navMenu
-              : `${styles.navMenu} ${styles.active}`
-          }
-        >
-          <li className={styles.navItem} onClick={() => setIsOpen(false)}>
-            <Link href='/' className={styles.navItem}>
-              {pathname === "/" ? (
-                <>
-                  <Arrow className={styles.icon} /> 
-                  Home
-                </>
-              ) : (
-                "Home"
-              )}
-            </Link>
-          </li>
-          {navItems.map((navItem, index) => (
-            <li
-              key={index}
-              className={styles.navItem}
-              onClick={() => setIsOpen(false)}
-            >
-              <Link href={navItem.href} className={styles.navItem}>
-                {pathname.includes(navItem.href) && (
+      <header className={styles.header}>
+        <nav className={styles.navbar}>
+          <ul
+            className={
+              isOpen === false
+                ? styles.navMenu
+                : `${styles.navMenu} ${styles.active}`
+            }
+          >
+            <li className={styles.navItem} onClick={() => setIsOpen(false)}>
+              <Link href='/' className={styles.navItem}>
+                {pathname === "/" ? (
                   <>
                     <Arrow className={styles.icon} />
-                    {navItem.text}
+                    Home
                   </>
+                ) : (
+                  "Home"
                 )}
-                {!pathname.includes(navItem.href) && navItem.text}{" "}
               </Link>
             </li>
-          ))}
+            {navItems.map((navItem, index) => (
+              <li
+                key={index}
+                className={styles.navItem}
+                onClick={() => setIsOpen(false)}
+              >
+                <Link href={navItem.href} className={styles.navItem}>
+                  {pathname.includes(navItem.href) && (
+                    <>
+                      <Arrow className={styles.icon} />
+                      {navItem.text}
+                    </>
+                  )}
+                  {!pathname.includes(navItem.href) && navItem.text}{" "}
+                </Link>
+              </li>
+            ))}
+            {isOpen && (
+              <div className={styles.btnContainerMobile}>
+                <Button
+                  href={"/"}
+                  text='Contact Us'
+                  btnType='secondary'
+                  arrow
+                />
+                <Button
+                  href={"/"}
+                  text='Schedule a call'
+                  btnType='secondary'
+                  arrow
+                />
+              </div>
+            )}
+          </ul>
           {isOpen && (
-            <div className={styles.btnContainerMobile}>
-              <Button href={"/"} text='Contact Us' btnType='secondary' arrow />
-              <Button
-                href={"/"}
-                text='Schedule a call'
-                btnType='secondary'
-                arrow
-              />
-            </div>
+            <div
+              className={`${styles.overlay} ${
+                isOverlayVisible ? styles.visible : ""
+              }`}
+              onClick={() => {
+                setIsOpen(false);
+                setIsOverlayVisible(false);
+              }}
+            ></div>
           )}
-        </ul>
-        {isOpen && (
-          <div
-            className={`${styles.overlay} ${
-              isOverlayVisible ? styles.visible : ""
-            }`}
-            onClick={() => {
-              setIsOpen(false);
-              setIsOverlayVisible(false);
-            }}
-          ></div>
-        )}
-        <div className={styles.btnContainer}>
-          <Button href={"/"} text='Contact Us' btnType='secondaryNav' />
-          <Button href={"/"} text='Schedule a call' btnType='primaryNav' />
-        </div>
-        <span
-          className={
-            isOpen === false
-              ? styles.hamburger
-              : `${styles.hamburger} ${styles.active}`
-          }
-          onClick={openMenu}
-        >
-          <span className={styles.whiteBar}></span>
-          <span className={styles.whiteBar}></span>
-          <span className={styles.whiteBar}></span>
-        </span>
-      </nav>
-    </header>
+          <div className={styles.btnContainer}>
+            <Button href={"/"} text='Contact Us' btnType='secondaryNav' />
+            <Button href={"/"} text='Schedule a call' btnType='primaryNav' />
+          </div>
+          <span
+            className={
+              isOpen === false
+                ? styles.hamburger
+                : `${styles.hamburger} ${styles.active}`
+            }
+            onClick={openMenu}
+          >
+            <span className={styles.whiteBar}></span>
+            <span className={styles.whiteBar}></span>
+            <span className={styles.whiteBar}></span>
+          </span>
+        </nav>
+      </header>
+    </>
   );
 }
 export default Nav;
