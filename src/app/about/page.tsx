308f171d9dccd3419ffd7281998e206dc14fd542
Chris Ware <c.p.ware@hotmail.com>
2024-03-14 22:43:10 -0700

    About update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
-import AboutGrid from "@/components/AboutGrid/AboutGrid";
 import AboutScrollText from "@/components/AboutScrollText/AboutScrollText";
 import PageIntro from "@/components/PageIntro/PageIntro";
 import { client } from "@/lib/sanity";
 import { simpleBlogCard } from "@/lib/interface";
 import BlogSection from "@/components/BlogSection/BlogSection";
+import ScrollHorizontalText from "@/components/ScrollHorizontalText/ScrollHorizontalText";
 
 async function getData() {
   const query = `
@@ -32,8 +32,8 @@ export default async function AboutPage() {
         span='Fonts & Footers'
         copy="We're on a mission to redefine the vacation rental experience by empowering property owners to seize control of their bookings. Discover the narrative behind our commitment to personalized, direct booking websites and join us in transforming the way you connect with your guests."
       />
+      <ScrollHorizontalText />
       <AboutScrollText />
-      {/* <AboutGrid /> */}
       <BlogSection data={data} />
     </>
   );
