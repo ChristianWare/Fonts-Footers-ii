d1b00cc1243882adb408155a93e521abf93a17a2
Chris Ware <c.p.ware@hotmail.com>
2024-03-16 21:47:39 -0700

    New BD Color

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,62 +11,69 @@ import Facebook from "../../../public/icons/facebook.svg";
 
 const Hero = () => {
   return (
-    <LayoutWrapper>
-      <section className={styles.container}>
-        <div className={styles.left}>
-          <h1 className={styles.heading}>
-            <span className={styles.span}>Direct Booking</span> Solutions
-          </h1>
-          <div className={styles.arrowContainer}>
-            <ArrowCluster />
+    <div className={styles.parent}>
+      <LayoutWrapper>
+        <section className={styles.container}>
+          <div className={styles.left}>
+            <h1 className={styles.heading}>
+              <span className={styles.span}>Direct Booking</span> Solutions
+            </h1>
+            <div className={styles.arrowContainer}>
+              <ArrowCluster />
+            </div>
+            <p className={styles.copy}>
+              Tired of relying solely on platforms like Airbnb or VRBO? The
+              Fonts & Footers web design agency empowers property owners to take
+              control of their bookings with a custom website to showcase thier
+              unique offerings.
+            </p>
+            <div className={styles.btnContainer}>
+              <Button
+                text='Schedule a call'
+                href='/'
+                btnType='primaryii'
+                arrow
+              />
+              <Button text='view our work' href='/' btnType='secondary' arrow />
+            </div>
+            <div className={styles.leftBottom}>
+              <Link
+                href='https://www.linkedin.com/'
+                target='_blank'
+                className={styles.detail}
+              >
+                <LinkedIn className={styles.icon} width={15} height={15} />
+              </Link>
+              <Link
+                href='https://www.facebook.com/'
+                target='_blank'
+                className={styles.detail}
+              >
+                <Facebook className={styles.icon} width={15} height={15} />
+              </Link>
+              <Link
+                href='https://instagram.com/'
+                target='_blank'
+                className={styles.detail}
+              >
+                <Insta className={styles.icon} width={15} height={15} />
+              </Link>
+            </div>
           </div>
-          <p className={styles.copy}>
-            Tired of relying solely on platforms like Airbnb or VRBO? The Fonts
-            & Footers web design agency empowers property owners to take control
-            of their bookings with a custom website to showcase thier unique
-            offerings.
-          </p>
-          <div className={styles.btnContainer}>
-            <Button text='Schedule a call' href='/' btnType='primaryii' arrow />
-            <Button text='view our work' href='/' btnType='secondary' arrow />
+          <div className={styles.right}>
+            <div className={styles.imgContainer}>
+              <Image
+                src={Img1}
+                alt='Direct Booking Website'
+                fill
+                priority
+                className={styles.img}
+              />
+            </div>
           </div>
-          <div className={styles.leftBottom}>
-            <Link
-              href='https://www.linkedin.com/'
-              target='_blank'
-              className={styles.detail}
-            >
-              <LinkedIn className={styles.icon} width={15} height={15} />
-            </Link>
-            <Link
-              href='https://www.facebook.com/'
-              target='_blank'
-              className={styles.detail}
-            >
-              <Facebook className={styles.icon} width={15} height={15} />
-            </Link>
-            <Link
-              href='https://instagram.com/'
-              target='_blank'
-              className={styles.detail}
-            >
-              <Insta className={styles.icon} width={15} height={15} />
-            </Link>
-          </div>
-        </div>
-        <div className={styles.right}>
-          <div className={styles.imgContainer}>
-            <Image
-              src={Img1}
-              alt='Direct Booking Website'
-              fill
-              priority
-              className={styles.img}
-            />
-          </div>
-        </div>
-      </section>
-    </LayoutWrapper>
+        </section>
+      </LayoutWrapper>
+    </div>
   );
 };
 export default Hero;
