7be260637ccc3b8fb4fbaee9d5693063cfdc0774
Chris Ware <c.p.ware@hotmail.com>
2024-03-26 18:52:12 -0700

    Hero copy update

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,69 +11,62 @@ import Facebook from "../../../public/icons/facebook.svg";
 
 const Hero = () => {
   return (
-    <div className={styles.parent}>
-      <LayoutWrapper>
-        <section className={styles.container}>
-          <div className={styles.left}>
-            <h1 className={styles.heading}>
-              <span className={styles.span}>Direct Booking</span> Solutions
-            </h1>
-            <div className={styles.arrowContainer}>
-              <ArrowCluster />
-            </div>
-            <p className={styles.copy}>
-              Tired of relying solely on platforms like Airbnb or VRBO? The
-              Fonts & Footers web design agency empowers property owners to take
-              control of their bookings with a custom website to showcase thier
-              unique offerings.
-            </p>
-            <div className={styles.btnContainer}>
-              <Button
-                text='Schedule a call'
-                href='/'
-                btnType='primaryii'
-                arrow
-              />
-              <Button text='view our work' href='/' btnType='secondary' arrow />
-            </div>
-            <div className={styles.leftBottom}>
-              <Link
-                href='https://www.linkedin.com/'
-                target='_blank'
-                className={styles.detail}
-              >
-                <LinkedIn className={styles.icon} width={15} height={15} />
-              </Link>
-              <Link
-                href='https://www.facebook.com/'
-                target='_blank'
-                className={styles.detail}
-              >
-                <Facebook className={styles.icon} width={15} height={15} />
-              </Link>
-              <Link
-                href='https://instagram.com/'
-                target='_blank'
-                className={styles.detail}
-              >
-                <Insta className={styles.icon} width={15} height={15} />
-              </Link>
-            </div>
+    <LayoutWrapper>
+      <section className={styles.container}>
+        <div className={styles.left}>
+          <h1 className={styles.heading}>
+            Empower your property with a{" "}
+            <span className={styles.span}>Direct Booking</span> Website
+          </h1>
+          <div className={styles.arrowContainer}>
+            <ArrowCluster />
           </div>
-          <div className={styles.right}>
-            <div className={styles.imgContainer}>
-              <Image
-                src={Img1}
-                alt='Direct Booking Website'
-                fill
-                priority
-                className={styles.img}
-              />
-            </div>
+          <p className={styles.copy}>
+            Tired of using sites like Air BNB or VRBO for your short-term rental
+            properties? Have your guests book directly with you to give them the
+            best hospitality experience.
+          </p>
+          <div className={styles.btnContainer}>
+            <Button text='Schedule a call' href='/' btnType='primaryii' arrow />
+            <Button text='view our work' href='/' btnType='secondary' arrow />
           </div>
-        </section>
-      </LayoutWrapper>
-    </div>
+          <div className={styles.leftBottom}>
+            <Link
+              href='https://www.linkedin.com/'
+              target='_blank'
+              className={styles.detail}
+            >
+              <LinkedIn className={styles.icon} width={15} height={15} />
+            </Link>
+            <Link
+              href='https://www.facebook.com/'
+              target='_blank'
+              className={styles.detail}
+            >
+              <Facebook className={styles.icon} width={15} height={15} />
+            </Link>
+            <Link
+              href='https://instagram.com/'
+              target='_blank'
+              className={styles.detail}
+            >
+              <Insta className={styles.icon} width={15} height={15} />
+            </Link>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.imgContainer}>
+            <Image
+              src={Img1}
+              alt='Direct Booking Website'
+              fill
+              priority
+              className={styles.img}
+            />
+          </div>
+        </div>
+      </section>
+    </LayoutWrapper>
   );
 };
 export default Hero;
