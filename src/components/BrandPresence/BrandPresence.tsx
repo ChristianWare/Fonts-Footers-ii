09817587c2616dd9082b87842db3d78605fce0b7
Chris Ware <c.p.ware@hotmail.com>
2024-03-29 11:30:19 -0700

    Brand Done

--- src/components/BrandPresence/BrandPresence.tsx
+++ src/components/BrandPresence/BrandPresence.tsx
@@ -1,11 +1,66 @@
 import LayoutWrapper from "../LayoutWrapper";
 import styles from "./BrandPresence.module.css";
+import House from "../../../public/icons/house.svg";
 
 const BrandPresence = () => {
+  const data = [
+    {
+      icon: <House width={30} height={30} className={styles.icon} />,
+      title: "Branding Control",
+      description:
+        "With a direct booking website, you have complete control over the branding elements, including logos, colors, fonts, and messaging. This ensures consistency with the overall brand identity and creates a cohesive experience for visitors.",
+    },
+    {
+      icon: <House width={30} height={30} className={styles.icon} />,
+      title: "Unique Content",
+      description:
+        "Owners can showcase their property's unique features, amenities, and attractions through compelling visual content, descriptions, and virtual tours. This helps differentiate the brand from competitors and highlights the property's value proposition.",
+    },
+    {
+      icon: <House width={30} height={30} className={styles.icon} />,
+      title: "Personalization",
+      description:
+        "Direct booking websites enable personalized communication and experiences for guests. Owners can greet visitors by name, recommend tailored recommendations based on their preferences, and offer customized packages or promotions, enhancing brand loyalty and satisfaction.",
+    },
+    {
+      icon: "",
+      title: "",
+      description: "",
+    },
+    {
+      icon: <House width={30} height={30} className={styles.icon} />,
+      title: "Guest Reviews",
+      description:
+        "Integrating guest reviews and testimonials on the website provides social proof and builds credibility. Positive reviews from satisfied guests enhance trust and encourage new visitors to book directly with confidence.",
+    },
+    {
+      icon: <House width={30} height={30} className={styles.icon} />,
+      title: "Online Visibility",
+      description:
+        "Optimizing the direct booking website for search engines improves online visibility and attracts organic traffic. By targeting relevant keywords, creating valuable content, and implementing SEO best practices, owners can increase brand exposure and reach a broader audience.",
+    },
+  ];
   return (
     <section>
       <LayoutWrapper>
-        <h2 className={styles.heading}>Brand Presence</h2>
+        <div className={styles.top}>
+          <h2 className={styles.heading}>Brand Presence</h2>
+          <p className={styles.copy}>
+            A well-designed and strategically optimized direct booking website
+            serves as a powerful tool for building and strengthening brand
+            presence, attracting guests, and fostering long-term brand loyalty
+            in the vacation rental industry.
+          </p>
+        </div>
+        <div className={styles.bottom}>
+          {data.map((x, index) => (
+            <div className={styles.card} key={index}>
+              {x.icon && <div className={styles.iconContainer}>{x.icon}</div>}
+              <h3 className={styles.title}>{x.title}</h3>
+              <p className={styles.desc}>{x.description}</p>
+            </div>
+          ))}
+        </div>
       </LayoutWrapper>
     </section>
   );
--- src/components/HigherMargins/HigherMargins.tsx
+++ src/components/HigherMargins/HigherMargins.tsx
@@ -11,32 +11,26 @@ const HigherMargins = () => {
       icon: <House width={30} height={30} className={styles.icon} />,
       title: "Elimination of Third-Party Fees",
       description:
-        "Direct bookings bypass commission fees charged by third-party booking platforms, allowing owners to keep a larger portion of the rental income.",
+        "Bypass commission fees charged by third-party booking platforms and keep a larger portion of the rental income.",
     },
     {
       src: Img,
       icon: <House width={30} height={30} className={styles.icon} />,
       title: "Control Over Pricing",
       description:
-        "Owners can set their own pricing without being constrained by platform fees or price restrictions imposed by third-party sites. This flexibility enables owners to adjust prices based on demand, seasonality, and market conditions, maximizing revenue potential.",
+        "Set your own pricing without being constrained by platform fees or price restrictions imposed by third-party sites. Adjust prices based on demand, seasonality, and market conditions, maximizing revenue potential.",
     },
-    // {
-    //   icon: <House width={30} height={30} className={styles.icon} />,
-    //   title: "Control Over Pricing",
-    //   description:
-    //     "Owners can set their own pricing without being constrained by platform fees or price restrictions imposed by third-party sites. This flexibility enables owners to adjust prices based on demand, seasonality, and market conditions, maximizing revenue potential.",
-    // },
     {
       icon: <House width={30} height={30} className={styles.icon} />,
       title: "Promotional Offers",
       description:
-        "Owners can create and promote special offers, discounts, and packages directly on their website to attract guests and increase bookings. Without third-party platform restrictions, owners can implement targeted promotions that align with their marketing strategies and revenue goals.",
+        "Create and promote special offers, discounts, and packages directly on your website to attract guests and increase bookings. Without third-party platform restrictions, you can implement targeted promotions that align with your marketing strategies and revenue goals.",
     },
     {
       icon: <House width={30} height={30} className={styles.icon} />,
       title: "Repeat Bookings",
       description:
-        "Direct communication with guests fosters stronger relationships and encourages repeat bookings. By offering excellent service, personalized experiences, and loyalty incentives, owners can increase guest retention and generate recurring revenue without incurring additional booking fees.",
+        "Direct communication with guests fosters stronger relationships and encourages repeat bookings. By offering excellent service, personalized experiences, and loyalty incentives, you can increase guest retention and generate recurring revenue without incurring additional booking fees.",
     },
     {
       src: Img2,
