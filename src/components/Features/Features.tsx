3a771fe2d71d8404d7c696d5f38cd665b42efe8d
Chris Ware <c.p.ware@hotmail.com>
2024-03-28 17:17:55 -0700

    c

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,11 +1,42 @@
 import LayoutWrapper from "../LayoutWrapper";
 import styles from "./Features.module.css";
-import { features } from "@/lib/data";
+// import { features } from "@/lib/data";
 import Image from "next/image";
 import Button from "../Button/Button";
 import House from "../../../public/images/hero2.png";
+import Calendar from "../../../public//icons/calendar.svg";
+import Admin from "../../../public//icons/admin.svg";
+import Review from "../../../public//icons/review.svg";
+import CreditCard from "../../../public//icons/creditCard.svg";
 
 const Features = () => {
+  const features = [
+    {
+      service: "Booking Engine",
+      description:
+        "Implement a user-friendly booking system that allows guests to check availability, select dates, and make reservations directly on your site.",
+      icon: <Calendar width={40} height={40} />,
+    },
+    {
+      service: "Guest Reviews",
+      description:
+        "Showcase positive reviews and testimonials from previous guests to build trust and credibility. Visitors will only be able to review once they've booked a stay on your property.",
+      icon: <Review width={40} height={40} />,
+    },
+    {
+      service: "Admin Panel",
+      description:
+        "Your admin panel will allow you to create, update, and delete listings on your personalized booking websites. View the number of bookings, and make any updates as needed.",
+      icon: <Admin width={40} height={40} />,
+    },
+    {
+      service: "Payment Processing",
+      description:
+        "We use Stripe payment processing so that you'll be able to secure online transactions, integrating a trusted payment gateway for businesses to accept credit card payments.",
+      icon: <CreditCard width={40} height={40} />,
+    },
+  ] as const;
+
   return (
     <section className={styles.parent}>
       <LayoutWrapper>
@@ -18,7 +49,7 @@ const Features = () => {
               <div className={styles.test}>
                 {features.slice(0, 4).map((x, index) => (
                   <div key={index} className={styles.featureContainer}>
-                    <Image src={x.icon} alt='icon' width={30} height={30} />
+                    {x.icon}
                     <h3 className={styles.service}>{x.service}</h3>
                     <p className={styles.description}>{x.description}</p>
                   </div>
