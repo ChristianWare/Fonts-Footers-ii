d49444b0ce77728d4f6092075bb598a49313ce36
Chris Ware <c.p.ware@hotmail.com>
2024-03-30 02:36:27 -0700

    c

--- src/components/FeaturesPageIntro/FeaturesPageIntro.tsx
+++ src/components/FeaturesPageIntro/FeaturesPageIntro.tsx
@@ -3,23 +3,31 @@ import styles from "./FeaturesPageIntro.module.css";
 import Image from "next/image";
 import Img from "../../../public/images/house23.png";
 import House from "../../../public/icons/house.svg";
+import ArrowCluster from "../ArrowCluster/ArrowCluster";
 
 const FeaturesPageIntro = () => {
   return (
     <section className={styles.container}>
       <LayoutWrapper>
         <div className={styles.top}>
-          <h1 className={styles.heading}>
-            {" "}
-            <span className={styles.span}>Features</span> of our{" "}
-            Direct Booking Websites
-          </h1>
-          <p className={styles.copy}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias,
-            quia porro. Porro, error dolore iure accusamus nulla nisi eaque1
-            quaerat magni adipisci minima pariatur ducimus. Debitis illo alias
-            officia minus.
-          </p>
+          <div className={styles.box1}>
+            <h1 className={styles.heading}>
+              {" "}
+              <span className={styles.span}>Features</span> of our Direct
+              Booking Websites
+            </h1>
+          </div>
+          <div className={styles.box2}>
+            <div className={styles.arrowContainer}>
+              <ArrowCluster />
+            </div>
+            <p className={styles.copy}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias,
+              quia porro. Porro, error dolore iure accusamus nulla nisi eaque1
+              quaerat magni adipisci minima pariatur ducimus. Debitis illo alias
+              officia minus.
+            </p>
+          </div>
         </div>
         <div className={styles.bottom}>
           <div className={styles.bottomLeft}>
