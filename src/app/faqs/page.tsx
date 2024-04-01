caa5703a10e6df8c1e10d2c12b2459ff731b86de
Chris Ware <c.p.ware@hotmail.com>
2024-04-01 11:31:48 -0700

    Faq page done

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -1,6 +1,28 @@
+import BlogSection from "@/components/BlogSection/BlogSection";
+import Faqsii from "@/components/Faqsii/Faqsii";
 import PageIntro from "@/components/PageIntro/PageIntro";
+import Pricing from "@/components/Pricing/Pricing";
+import { simpleBlogCard } from "@/lib/interface";
+import { client } from "@/lib/sanity";
 
-export default function FAQPage() {
+async function getData() {
+  const query = `
+    *[_type == 'blog'][0...2] | order(_createdAt desc) {
+      title,
+      smallDescription,
+      publishedAt,
+      "currentSlug": slug.current,
+      titleImage
+    }`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function FAQPage() {
+  const data: simpleBlogCard[] = await getData();
+
   return (
     <>
       <PageIntro
@@ -11,6 +33,9 @@ export default function FAQPage() {
         copy="Here are some commonly asked questions and their answers below. If you don't see your questions here, call us any time."
         copyColor='green'
       />
+      <Faqsii />
+      <Pricing />
+      <BlogSection data={data} />
     </>
   );
 }
---
+++ src/components/Faqsii/Faqsii.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useState } from "react";
+import LayoutWrapper from "../LayoutWrapper";
+import { faqs } from "@/lib/data";
+import Arrow2 from "../../../public/icons/arrow2.svg";
+import House from "../../../public/images/faqs.png";
+import Image from "next/image";
+import styles from "./Faqsii.module.css";
+import ArrowCluster from "../ArrowCluster/ArrowCluster";
+import Button from "../Button/Button";
+
+const Faqsii = () => {
+  const [selected, setSelected] = useState<null | number>(null);
+
+  const toggle = (i: any) => {
+    if (selected === i) {
+      return setSelected(null);
+    }
+
+    setSelected(i);
+  };
+  return (
+    <section className={styles.container}>
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <div className={styles.bottom}>
+            <div className={styles.right}>
+              {faqs.map((x, i) => (
+                <div
+                  key={x.id}
+                  className={styles.qaContainer}
+                  onClick={() => toggle(i)}
+                >
+                  <div className={styles.headingArrowContainer}>
+                    <div className={styles.h3Container}>
+                      <h3 className={styles.question} lang='en'>
+                        {x.question}
+                      </h3>
+                    </div>
+                    {selected === i ? (
+                      <div className={styles.iconBox}>
+                        <Arrow2
+                          className={styles.iconFlip}
+                          width={25}
+                          height={25}
+                        />
+                      </div>
+                    ) : (
+                      <div className={styles.iconBox}>
+                        <Arrow2
+                          className={styles.icon}
+                          width={25}
+                          height={25}
+                        />
+                      </div>
+                    )}
+                  </div>
+                  <div
+                    className={
+                      selected === i
+                        ? styles.answerContainer + " " + styles.show
+                        : styles.answerContainer
+                    }
+                  >
+                    <p className={styles.answer} lang='en'>
+                      {x.answer}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </LayoutWrapper>
+    </section>
+  );
+};
+export default Faqsii;
