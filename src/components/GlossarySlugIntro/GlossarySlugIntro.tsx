43cf923cf76864da8e07e629a85f83ae0923a1c4
Chris Ware <c.p.ware@hotmail.com>
2024-04-03 01:07:19 -0700

    mdx files added

---
+++ src/components/GlossarySlugIntro/GlossarySlugIntro.tsx
@@ -0,0 +1,32 @@
+import ContentPadding from "../ContentPadding/ContentPadding";
+import LayoutWrapper from "../LayoutWrapper";
+import styles from "./GlossarySlugIntro.module.css";
+import { FC } from "react";
+
+interface Props {
+  heading: string;
+  description: string;
+  date: string;
+  category: string;
+}
+
+const GlossarySlugIntro: FC<Props> = ({
+  heading,
+  description,
+  date,
+  category,
+}) => {
+  return (
+    <section className={styles.container}>
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <h1 className={styles.heading}>{heading}</h1>
+            <p className={styles.copy}>{description}</p>
+          </div>
+        </div>
+      </LayoutWrapper>
+    </section>
+  );
+};
+export default GlossarySlugIntro;
