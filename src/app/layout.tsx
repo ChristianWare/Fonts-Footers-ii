0ea89ed836bfac018df82d44fe52c3bf7cd488b7
Chris Ware <c.p.ware@hotmail.com>
2024-04-23 23:12:08 -0700

    removed GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,6 @@ import Nav from "@/components/Nav/Nav";
 import FinalCta from "@/components/FinalCTA/FinalCTA";
 import CallToAction from "@/components/CallToAction/CallToAction";
 import { Toaster } from "react-hot-toast";
-import { GoogleAnalytics } from "@next/third-parties/google";
-import Script from "next/script";
 
 const oswald = Oswald({
   subsets: ["latin"],
@@ -48,19 +46,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <head>
-        <Script
-          id='gtm'
-          strategy='afterInteractive'
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PFXJQHBD');`,
-          }}
-        ></Script>
-      </head>
+      
       <body
         className={`${oswald.variable} ${raleway.variable} ${adedisplay.variable}`}
       >
@@ -84,7 +70,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           <FinalCta />
         </NoiseBg>
       </body>
-      <GoogleAnalytics gaId='G-93GBMVZ4E0' />
     </html>
   );
 }
