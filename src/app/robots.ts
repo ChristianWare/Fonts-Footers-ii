ddb49a1ea886a694254b7a9118af6999499a967e
Chris Ware <c.p.ware@hotmail.com>
2024-04-24 14:19:54 -0700

    Analytics

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/admin", "/privacy"],
+        disallow: ["/admin", "/privacy", "/analytics"],
       },
     ],
     sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
