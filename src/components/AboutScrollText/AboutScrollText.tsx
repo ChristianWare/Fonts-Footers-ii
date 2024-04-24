58b9d8d1a24d22b7d24c76d21d804828f1817b63
Chris Ware <c.p.ware@hotmail.com>
2024-04-24 00:00:01 -0700

    gsap removed

--- src/components/AboutScrollText/AboutScrollText.tsx
+++ src/components/AboutScrollText/AboutScrollText.tsx
@@ -4,63 +4,17 @@ import Author from "../../../public/images/author.png";
 import Image from "next/image";
 
 const AboutScrollText = () => {
-  // const [lettersRef, setLettersRef] = useArrayRef();
-
-  // function useArrayRef(): [
-  //   React.MutableRefObject<HTMLSpanElement[]>,
-  //   (ref: HTMLSpanElement) => void
-  // ] {
-  //   const lettersRef = useRef<HTMLSpanElement[]>([]);
-  //   lettersRef.current = [];
-  //   return [lettersRef, (ref) => ref && lettersRef.current.push(ref)];
-  // }
-
-  // const triggerRef = useRef(null);
-
-  // gsap.registerPlugin(ScrollTrigger);
-
-  // const text =
-  //   '" Our vision is for Homeowners to connect directly with travelers, without relying on platforms like Airbnb. We  help you maximizing revenue, build guest relationships, and thrive in the home rental industry. "';
-
-  // useEffect(() => {
-  //   const anim = gsap.to(lettersRef.current, {
-  //     scrollTrigger: {
-  //       trigger: triggerRef.current,
-  //       scrub: 0.9,
-  //       start: "top center",
-  //       end: "bottom 85%",
-  //     },
-  //     color: "#aeff12",
-  //     duration: 3,
-  //     stagger: 1,
-  //   });
-  //   return () => {
-  //     anim.kill();
-  //   };
-  // }, [lettersRef]);
-
   return (
     <section className={styles.bgColor}>
       <LayoutWrapper color='green3'>
         <div className={styles.content}>
           <div className={styles.reveal}>
-            {/* <div ref={triggerRef}> */}
             <h2 className={styles.text}>
-              {/* {text.split("").map((letter, index) => (
-                <span
-                  key={index}
-                  className={styles.revealText}
-                  ref={setLettersRef}
-                >
-                  {letter}
-                </span>
-              ))} */}
-              Our vision is for Homeowners to connect directly with travelers,
-              without relying on platforms like Airbnb. We help you maximizing
-              revenue, build guest relationships, and thrive in the home rental
-              industry.
+              &ldquo;Our vision is for Homeowners to connect directly with
+              travelers, without relying on platforms like Airbnb. We help you
+              maximizing revenue, build guest relationships, and thrive in the
+              home rental industry.&rdquo;
             </h2>
-            {/* </div> */}
           </div>
           <div className={styles.bottom}>
             <Image
--- src/components/ScrollHorizontalText/ScrollHorizontalText.tsx
+++ src/components/ScrollHorizontalText/ScrollHorizontalText.tsx
@@ -1,50 +1,13 @@
 "use client";
 
-import { gsap } from "gsap";
 import styles from "./ScrollHorizontalText.module.css";
-import { useEffect, useRef } from "react";
-import { ScrollTrigger } from "gsap/all";
 import Arrow from "../../../public/icons/arrow.svg";
 
 const ScrollHorizontalText = () => {
-  const firstText = useRef(null);
-  const secondText = useRef(null);
-  const slider = useRef(null);
-  let xPercent = 0;
-  let direction = -1;
-
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to(slider.current, {
-      scrollTrigger: {
-        trigger: document.documentElement,
-        scrub: 0.25,
-        start: 0,
-        end: window.innerHeight,
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        onUpdate: (e) => (direction = e.direction * -1),
-      },
-      x: "-500px",
-    });
-    requestAnimationFrame(animate);
-  }, []);
-
-  const animate = () => {
-    if (xPercent < -100) {
-      xPercent = 0;
-    } else if (xPercent > 0) {
-      xPercent = -100;
-    }
-    gsap.set(firstText.current, { xPercent: xPercent });
-    gsap.set(secondText.current, { xPercent: xPercent });
-    requestAnimationFrame(animate);
-    xPercent += 0.03 * direction;
-  };
-
   return (
     <section className={styles.sliderContainer}>
-      <div ref={slider} className={styles.slider}>
-        <h2 ref={firstText}>
+      <div className={styles.slider}>
+        <h2>
           Fonts & Footers{" "}
           <span>
             <Arrow className={styles.icon2} />
@@ -86,7 +49,7 @@ const ScrollHorizontalText = () => {
             <Arrow className={styles.icon2} />
           </span>{" "}
         </h2>
-        <h2 ref={secondText}>
+        <h2>
           Fonts & Footers{" "}
           <span>
             <Arrow className={styles.icon2} />
