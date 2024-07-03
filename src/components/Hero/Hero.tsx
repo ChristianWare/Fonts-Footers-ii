248b908ed5b6e3faa6fe3d5a89f79f172792b6ee
Chris Ware <c.p.ware@hotmail.com>
2024-07-03 09:02:47 -0700

    New Tan BG Color

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,42 +10,43 @@ import ShuffleHero from "../ShuffleHero/ShuffleHero";
 
 const Hero = () => {
   return (
-    <LayoutWrapper>
-      <motion.section
-        initial={{ y: -100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        className={styles.container}
-      >
-        <div className={styles.left}>
-          <h1 className={styles.heading}>
-            We build <span className={styles.span1}>interactive</span> and{" "}
-            <span className={styles.span2}> immersive</span> websites
-            <span className={styles.span3}>.</span>
-          </h1>
+    <section className={styles.container}>
+      <LayoutWrapper>
+        <motion.section
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          className={styles.content}
+        >
+          <div className={styles.left}>
+            <h1 className={styles.heading}>
+              We build <span className={styles.span1}>interactive</span> and{" "}
+              <span className={styles.span2}> immersive</span> websites
+              <span className={styles.span3}>.</span>
+            </h1>
 
-          <p className={styles.copy}>
-            Stop depending on sites like Air BNB or VRBO for short-term rental
-            listings. Enhance guest satisfaction, get repeat stays, and
-            eliminate all commission fees by having guests book directly with
-            you.
-          </p>
-          <div className={styles.btnContainer}>
-            <Button
-              text='Schedule a call'
-              href='/contact/#schedule'
-              btnType='primaryii'
-              arrow
-            />
-            {/* <Button
+            <p className={styles.copy}>
+              Stop depending on sites like Air BNB or VRBO for short-term rental
+              listings. Enhance guest satisfaction, get repeat stays, and
+              eliminate all commission fees by having guests book directly with
+              you.
+            </p>
+            <div className={styles.btnContainer}>
+              <Button
+                text='Schedule a call'
+                href='/contact/#schedule'
+                btnType='primaryii'
+                arrow
+              />
+              {/* <Button
               text='Direct Booking Benefits'
               href='/benefits'
               btnType='secondary'
               arrow
             /> */}
+            </div>
           </div>
-        </div>
-        <div className={styles.right}>
-          {/* <div className={styles.imgContainer}>
+          <div className={styles.right}>
+            {/* <div className={styles.imgContainer}>
             <Image
               src={Img1}
               alt='Direct Booking Website'
@@ -55,10 +56,11 @@ const Hero = () => {
               className={styles.img}
             />
           </div> */}
-          <ShuffleHero />
-        </div>
-      </motion.section>
-    </LayoutWrapper>
+            <ShuffleHero />
+          </div>
+        </motion.section>
+      </LayoutWrapper>
+    </section>
   );
 };
 export default Hero;
