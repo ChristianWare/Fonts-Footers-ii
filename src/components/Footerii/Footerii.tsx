7bc1d37ab4908dab0621142b157bbbb03485bd51
Chris Ware <c.p.ware@hotmail.com>
2024-07-03 18:00:08 -0700

    Footer Done

--- src/components/Footerii/Footerii.tsx
+++ src/components/Footerii/Footerii.tsx
@@ -5,7 +5,6 @@ import Button from "../Button/Button";
 import Insta from "../../../public/icons/instagram.svg";
 import LinkedIn from "../../../public/icons/linkedin.svg";
 import Facebook from "../../../public/icons/facebook.svg";
-import Logo from "../../../public/icons/fnflogo.svg";
 
 const Footerii = () => {
   const footer = [
@@ -66,107 +65,110 @@ const Footerii = () => {
     },
   ];
   return (
-    <footer>
-      <LayoutWrapper>
-        <div className={styles.content}>
-          <Link href='/' className={styles.logoMobile}>
-            FONTS & FOOTERS
-          </Link>
-          <div className={styles.top}>
-            <div className={styles.box}>
-              <Link href='/' className={styles.logo}>
-                FONTS & FOOTERS
-              </Link>
-            </div>
-            <div className={styles.box}>
-              {footer.map((x) => (
-                <div key={x.id}>
-                  <h5 className={styles.sectionHeading}>{x.heading}</h5>
-                  <ul className={styles.sectionList}>
-                    {x.section.map((y) => (
-                      <li key={y.id} className={styles.listItem}>
-                        <Link href={y.href}>{y.name}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-            </div>
-            <div className={styles.box}>
-              {footerii.map((x) => (
-                <div key={x.id}>
-                  <h5 className={styles.sectionHeading}>{x.heading}</h5>
-                  <ul className={styles.sectionList}>
-                    {x.section.map((y) => (
-                      <li key={y.id} className={styles.listItem}>
-                        <Link href={y.href}>{y.name}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-            </div>
-            <div className={styles.box}>
-              <h5 className={styles.sectionHeading}>Subscribe</h5>
-              <p className={styles.newslettterCopy}>
-                Join our newsletter to stay up to date on features and releases.
-              </p>
-              <form className={styles.form}>
-                <div className={styles.labelInputBox}>
-                  {/* <label htmlFor='firstName'>First Name</label> */}
-                  <input id='firstName' type='text' />
-                </div>
-                <div className={styles.btnContainer}>
-                  <Button btnType='primary' href='/' text='Subscribe' arrow />
-                  <small className={styles.small}>
-                    By subscribing you agree to with our Privacy Policy and
-                    provide consent to receive updates from our company.
-                  </small>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className={styles.bottom}>
-            <div className={styles.left}>
-              <small className={styles.small1}>
-                Another Website by <b>Fonts & Footers</b> |
-              </small>
-              <small className={styles.small1}>
-                © 2024 Fonts & Footers. All rights reserved. |
-              </small>
-              <small className={styles.small1}>Privacy Policy</small>
-            </div>
-            <div className={styles.right}>
-              <div className={styles.leftBottom}>
-                <Link
-                  href='https://www.linkedin.com/'
-                  target='_blank'
-                  aria-label='LinkedIn'
-                  className={styles.detail}
-                >
-                  <LinkedIn className={styles.icon} width={12} height={12} />
+    <footer className={styles.container}>
+      <div className={styles.parent}>
+        <LayoutWrapper>
+          <div className={styles.content}>
+            <Link href='/' className={styles.logoMobile}>
+              FONTS & FOOTERS
+            </Link>
+            <div className={styles.top}>
+              <div className={styles.box}>
+                <Link href='/' className={styles.logo}>
+                  FONTS & FOOTERS
                 </Link>
-                <Link
-                  href='https://www.facebook.com/'
-                  target='_blank'
-                  aria-label='Facebook'
-                  className={styles.detail}
-                >
-                  <Facebook className={styles.icon} width={12} height={12} />
-                </Link>
-                <Link
-                  href='https://instagram.com/'
-                  target='_blank'
-                  aria-label='Instagram'
-                  className={styles.detail}
-                >
-                  <Insta className={styles.icon} width={12} height={12} />
-                </Link>
+              </div>
+              <div className={styles.box}>
+                {footer.map((x) => (
+                  <div key={x.id}>
+                    <h5 className={styles.sectionHeading}>{x.heading}</h5>
+                    <ul className={styles.sectionList}>
+                      {x.section.map((y) => (
+                        <li key={y.id} className={styles.listItem}>
+                          <Link href={y.href}>{y.name}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+              <div className={styles.box}>
+                {footerii.map((x) => (
+                  <div key={x.id}>
+                    <h5 className={styles.sectionHeading}>{x.heading}</h5>
+                    <ul className={styles.sectionList}>
+                      {x.section.map((y) => (
+                        <li key={y.id} className={styles.listItem}>
+                          <Link href={y.href}>{y.name}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+              <div className={styles.box}>
+                <h5 className={styles.sectionHeading}>Subscribe</h5>
+                <p className={styles.newslettterCopy}>
+                  Join our newsletter to stay up to date on features and
+                  releases.
+                </p>
+                <form className={styles.form}>
+                  <div className={styles.labelInputBox}>
+                    {/* <label htmlFor='firstName'>First Name</label> */}
+                    <input id='firstName' type='text' />
+                  </div>
+                  <div className={styles.btnContainer}>
+                    <Button btnType='primary' href='/' text='Subscribe' arrow />
+                    <small className={styles.small}>
+                      By subscribing you agree to with our Privacy Policy and
+                      provide consent to receive updates from our company.
+                    </small>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className={styles.bottom}>
+              <div className={styles.left}>
+                <small className={styles.small1}>
+                  Another Website by <b>Fonts & Footers</b> |
+                </small>
+                <small className={styles.small1}>
+                  © 2024 Fonts & Footers. All rights reserved. |
+                </small>
+                <small className={styles.small1}>Privacy Policy</small>
+              </div>
+              <div className={styles.right}>
+                <div className={styles.leftBottom}>
+                  <Link
+                    href='https://www.linkedin.com/'
+                    target='_blank'
+                    aria-label='LinkedIn'
+                    className={styles.detail}
+                  >
+                    <LinkedIn className={styles.icon} width={12} height={12} />
+                  </Link>
+                  <Link
+                    href='https://www.facebook.com/'
+                    target='_blank'
+                    aria-label='Facebook'
+                    className={styles.detail}
+                  >
+                    <Facebook className={styles.icon} width={12} height={12} />
+                  </Link>
+                  <Link
+                    href='https://instagram.com/'
+                    target='_blank'
+                    aria-label='Instagram'
+                    className={styles.detail}
+                  >
+                    <Insta className={styles.icon} width={12} height={12} />
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </LayoutWrapper>
+        </LayoutWrapper>
+      </div>
     </footer>
   );
 };
