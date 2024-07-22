a2f2172a97876c280fb96b1dac740ebd9e37d285
Chris Ware <c.p.ware@hotmail.com>
2024-07-21 21:51:22 -0700

    c

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -48,8 +48,8 @@ export default async function AboutPage() {
       <PageIntro
         text='About'
         span='Fonts & Footers'
-        copy="We're on a mission to redefine the vacation rental experience by empowering property owners to seize control of their bookings. Discover the narrative behind our commitment to personalized, direct booking websites and join us in transforming the way you connect with your guests."
         src={Img}
+        orientation='center'
       />
       <AboutScrollText />
       <ScrollHorizontalText text='Our Clients' bottomBorder={true} />
