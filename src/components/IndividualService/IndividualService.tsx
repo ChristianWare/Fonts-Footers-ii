5c2166b2ed769dc977db8d592b9ddb7cd6cd7f07
Chris Ware <c.p.ware@hotmail.com>
2024-07-24 02:47:32 -0700

    c

--- src/components/IndividualService/IndividualService.tsx
+++ src/components/IndividualService/IndividualService.tsx
@@ -18,23 +18,19 @@ const IndividualService = () => {
     //   whyImportant: [
     //     {
     //       title: "Secure Transactions",
-    //       modal:
-    //         "Stripe provides a secure platform for processing payments, ensuring that transactions are safe and protected against fraud.",
+    //      
     //     },
     //     {
     //       title: "Payment Methods",
-    //       modal:
-    //         "Guests can pay using various payment methods, including credit/debit cards, digital wallets, and bank transfers, accommodating different guest preferences.",
+    //      
     //     },
     //     {
     //       title: "Global Reach",
-    //       modal:
-    //         "Stripe supports international payments, allowing you to accept payments from guests worldwide and expand your customer base.",
+    //       
     //     },
     //     {
     //       title: "Live Reporting",
-    //       modal:
-    //         "You can access real-time payment reports, analytics, and insights through the Stripe dashboard, helping you track revenue, monitor transactions, and make data-driven decisions.",
+    //      
     //     },
     //   ],
     // },
@@ -47,24 +43,20 @@ const IndividualService = () => {
       whyImportant: [
         {
           title: "Control",
-          modal:
-            "With a booking engine, you have full control over pricing, availability, and booking policies. You can set dynamic pricing strategies, offer discounts or promotions, and adjust availability in real-time based on demand and seasonality.",
+         
         },
         {
           title: "Guest Experience",
-          modal:
-            "Providing a seamless and intuitive booking process through a booking engine enhances the guest experience. Guests can easily check availability, select dates, view property details, and complete bookings without navigating away from your website.",
+        
         },
         {
           title: "Customization",
-          modal:
-            "A booking engine allows for customization and personalization of the booking experience. You can tailor the booking process to match your brand's aesthetics, incorporate upselling opportunities, and collect guest preferences for personalized recommendations.",
+         
         },
-        {
-          title: "Streamlined Operations",
-          modal:
-            "Automating the booking process through a booking engine streamlines operations. It reduces manual tasks, such as processing bookings via phone or email, and minimizes the risk of overbooking or double bookings with real-time inventory management.",
-        },
+        // {
+        //   title: "Streamlined Operations",
+         
+        // },
       ],
     },
     {
@@ -76,24 +68,20 @@ const IndividualService = () => {
       whyImportant: [
         {
           title: "Transparency",
-          modal:
-            "Guest reviews provide transparency and authenticity, helping potential guests make informed decisions about booking your property.",
+         
         },
         {
           title: "Trust Building",
-          modal:
-            "Positive reviews build trust among potential guests, reassuring them about the quality of your property and services.",
+         
         },
         {
           title: "Feedback and Improvement",
-          modal:
-            "Guest reviews provide valuable feedback that can help you identify areas for improvement and enhance the overall guest experience.",
+          
         },
-        {
-          title: "Reputation Management",
-          modal:
-            "Managing guest reviews allows you to respond to feedback, address concerns, and showcase your commitment to guest satisfaction, enhancing your reputation.",
-        },
+        // {
+        //   title: "Reputation Management",
+         
+        // },
       ],
     },
     {
@@ -105,24 +93,20 @@ const IndividualService = () => {
       whyImportant: [
         {
           title: "Visual Representation",
-          modal:
-            " It provides a visual representation of property locations, nearby attractions, and amenities, making it easier for guests to understand the property's surroundings and plan their stay accordingly.",
+         
         },
         {
           title: "Improved Decision Making",
-          modal:
-            "Guests can assess the proximity of your properties to key destinations such as beaches, restaurants, shopping areas, and transportation hubs, helping them make informed booking decisions that align with their preferences and itinerary.",
+         
         },
         {
           title: "Increased Engagement",
-          modal:
-            "The interactive map feature increases user engagement on your website as guests interact with the map to discover nearby amenities, attractions, and points of interest, keeping them engaged and informed throughout their booking journey.",
+          
         },
-        {
-          title: "Enhanced Marketing",
-          modal:
-            "The interactive map can be leveraged as a marketing tool to highlight the unique selling points of your properties, such as proximity to popular tourist spots, scenic views, or exclusive access to amenities, attracting more bookings and inquiries.",
-        },
+        // {
+        //   title: "Enhanced Marketing",
+          
+        // },
       ],
     },
   ] as const;
@@ -145,7 +129,7 @@ const IndividualService = () => {
                 <p className={styles.desc}>{x.description}</p>
                 <br />
 
-                <span className={styles.span}>Benefits:</span>
+                <span className={styles.span}>Features:</span>
                 <ul className={styles.box}>
                   {x.whyImportant.map((y, index) => (
                     <li key={index} className={styles.reasonTitleContainer}>
