e598729dd98f08e0b9266a79cf721a7c9534e659
Chris Ware <c.p.ware@hotmail.com>
2024-07-24 17:14:47 -0700

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,13 @@ import Testimonials from "@/components/Testimonials/Testimonials";
 import { client } from "@/lib/sanity";
 import { simpleBlogCard } from "@/lib/interface";
 import ScrollHorizontalText from "@/components/ScrollHorizontalText/ScrollHorizontalText";
-import Challenges from "@/components/Challenges/Challenges";
-import Carouselii from "@/components/Carouselii/Carouselii";
 import Slider from "@/components/Slider/Slider";
 import ServicesSection from "@/components/ServicesSection/ServicesSection";
 import AboutScrollText from "@/components/AboutScrollText/AboutScrollText";
-import { Reviewsii } from "@/components/Reviewsii/Reviewsii";
 import Faqsiii from "@/components/Faqsiii/Faqsiii";
 import EveryPlan from "@/components/EveryPlan/EveryPlan";
 import HowItWorks from "@/components/HowItWorks/HowItWorks";
 import { homePageFaqs } from "@/lib/data";
-import Carousel from "@/components/Carousel/Carousel";
 import Usp from "@/components/Usp/Usp";
 
 async function getData() {
@@ -50,8 +46,6 @@ export default async function Home() {
       <Slider />
       <ScrollHorizontalText text='Our Clients' bottomBorder={false} />
       <AboutScrollText />
-      {/* <Benefits />  */}{" "}
-      {/* to be used on the money/individual services page */}
       <ServicesSection />
       <EveryPlan />
       <Usp />
