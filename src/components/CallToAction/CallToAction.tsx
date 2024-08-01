cb908ef50471ddeb3720399f44e9479b6310ddd5
Chris Ware <c.p.ware@hotmail.com>
2024-07-31 20:37:53 -0700

    REdesign complete

--- src/components/CallToAction/CallToAction.tsx
+++ src/components/CallToAction/CallToAction.tsx
@@ -16,6 +16,29 @@ const CallToAction = () => {
       <LayoutWrapper>
         <section className={styles.content}>
           <div className={styles.miscContainer}>
+          <svg
+            width='0'
+            height='0'
+            xmlns='http://www.w3.org/2000/svg'
+            version='1.1'
+          >
+            <defs>
+              <filter id='goo'>
+                <feGaussianBlur
+                  in='SourceGraphic'
+                  stdDeviation='5'
+                  result='blur'
+                />
+                <feColorMatrix
+                  in='blur'
+                  mode='matrix'
+                  values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9'
+                  result='goo'
+                />
+                <feComposite in='SourceGraphic' in2='goo' operator='atop' />
+              </filter>
+            </defs>
+          </svg>
             <div className={styles.imgContainer}>
               <Image
                 src={img}
