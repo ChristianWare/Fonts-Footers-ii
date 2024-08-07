1c0f1a095eb2b32a127a0a6dcc19d7bc32a7747d
Chris Ware <c.p.ware@hotmail.com>
2024-08-06 22:18:13 -0700

    About us update

--- src/components/AboutScrollText/AboutScrollText.tsx
+++ src/components/AboutScrollText/AboutScrollText.tsx
@@ -11,29 +11,29 @@ const AboutScrollText = () => {
         <div className={styles.content}>
           <div className={styles.left}>
             <div className={styles.miscContainer}>
-            <svg
-              width='0'
-              height='0'
-              xmlns='http://www.w3.org/2000/svg'
-              version='1.1'
-            >
-              <defs>
-                <filter id='goo'>
-                  <feGaussianBlur
-                    in='SourceGraphic'
-                    stdDeviation='5'
-                    result='blur'
-                  />
-                  <feColorMatrix
-                    in='blur'
-                    mode='matrix'
-                    values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9'
-                    result='goo'
-                  />
-                  <feComposite in='SourceGraphic' in2='goo' operator='atop' />
-                </filter>
-              </defs>
-            </svg>
+              <svg
+                width='0'
+                height='0'
+                xmlns='http://www.w3.org/2000/svg'
+                version='1.1'
+              >
+                <defs>
+                  <filter id='goo'>
+                    <feGaussianBlur
+                      in='SourceGraphic'
+                      stdDeviation='5'
+                      result='blur'
+                    />
+                    <feColorMatrix
+                      in='blur'
+                      mode='matrix'
+                      values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9'
+                      result='goo'
+                    />
+                    <feComposite in='SourceGraphic' in2='goo' operator='atop' />
+                  </filter>
+                </defs>
+              </svg>
               <div className={styles.imgContainer}>
                 <Image
                   src={Author}
@@ -46,10 +46,14 @@ const AboutScrollText = () => {
             </div>
           </div>
           <div className={styles.right}>
-            <div className={styles.falseBtnContainer}>
+            {/* <div className={styles.falseBtnContainer}>
               <FalseButton btnType='secondary' text='About us' />
-            </div>
-            <h2 className={`${styles.heading} h2v2`}>Who are we?</h2>
+            </div> */}
+            <h2 className={`${styles.heading} h2v2`}>
+              What is
+              <br />
+              <em> Fonts & Footers?</em>
+            </h2>
             <p className={styles.text}>
               Fonts & Footers is a digital agency focusing on web design, web
               development, and SEO. We&apos;re experts at blending the latest
@@ -57,9 +61,17 @@ const AboutScrollText = () => {
               with us, you get personal care, support, and a website that boosts
               engagement and sales. We opperate out of sunny Phoenix, AZ.
             </p>
-            <p className={styles.authorDetails}>
-              ~ Chris Ware, Founder & CEO ~
+            <br />
+            <p className={styles.text}>
+              We use cutting-edge technology to craft websites that outperform
+              those built on platforms like Wordpress, Wix, or Godaddy, which
+              often rely on older, slower, and outdated technology. Our approach
+              involves using pure HTML, CSS, and Javascript code, ensuring a
+              sleek and efficient website.
             </p>
+            {/* <p className={styles.authorDetails}>
+              ~ Chris Ware, Founder & CEO ~
+            </p> */}
           </div>
         </div>
       </LayoutWrapper>
