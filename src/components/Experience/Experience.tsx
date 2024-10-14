cffda9a1dd7aab6b95bda2e363c0c727c2f1fb2a
Chris Ware <c.p.ware@hotmail.com>
2024-10-14 12:54:04 -0700

    experience update

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -110,6 +110,47 @@ const Experience = () => {
               </div>
             ))}
           </div>
+          <div className={styles.mapContainer}>
+            {faqs.map((x, index) => (
+              <div
+                key={index}
+                className={styles.qaContainer}
+                onClick={() => toggle(index)}
+              >
+                <div className={styles.headingArrowContainer}>
+                  <div className={styles.h3Container}>
+                    <h3 className={styles.question} lang='en'>
+                      {x.question}
+                    </h3>
+                  </div>
+                  {selected === index ? (
+                    <div className={styles.iconBox}>
+                      <Arrow2
+                        className={styles.iconFlip}
+                        width={25}
+                        height={25}
+                      />
+                    </div>
+                  ) : (
+                    <div className={styles.iconBox}>
+                      <Arrow2 className={styles.icon} width={25} height={25} />
+                    </div>
+                  )}
+                </div>
+                <div
+                  className={
+                    selected === index
+                      ? styles.answerContainer + " " + styles.show
+                      : styles.answerContainer
+                  }
+                >
+                  <p className={styles.answer} lang='en'>
+                    {x.answer}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
